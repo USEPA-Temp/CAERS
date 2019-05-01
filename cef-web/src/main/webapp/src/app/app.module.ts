9c28c0b2e267335198480a2c8f409b2a9b8385e2
Thomas Fesperman <thomas.fesperman@cgifederal.com>
2019-05-01 10:28:25 -0400

    Structure changes for adding the sidebar

--- cef-web/src/main/webapp/src/app/app.module.ts
+++ cef-web/src/main/webapp/src/app/app.module.ts
@@ -6,27 +6,29 @@ import { HttpClientModule }    from '@angular/common/http';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { FacilityDashboardComponent } from './facility-dashboard/facility-dashboard.component';
-import { FacilityInfoComponent } from './components/facility-info/facility-info.component';
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { FacilityListComponent } from './facility-dashboard/facility-list/facility-list.component';
 import { NotificationListComponent } from './facility-dashboard/notification-list/notification-list.component';
 import { FacilityListItemComponent } from './facility-dashboard/facility-list/facility-list-item/facility-list-item.component';
+import { SharedModule } from './shared/shared.module';
+import { BreadcrumbNavComponent } from './components/breadcrumb-nav/breadcrumb-nav.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     FacilityDashboardComponent,
-    FacilityInfoComponent,
     HeaderComponent,
     FooterComponent,
     FacilityListComponent,
     NotificationListComponent,
-    FacilityListItemComponent
+    FacilityListItemComponent,
+    BreadcrumbNavComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
+    SharedModule,
     NgbModule,
     HttpClientModule
   ],
--- cef-web/src/main/webapp/src/app/components/header/header.component.ts
+++ cef-web/src/main/webapp/src/app/components/header/header.component.ts
@@ -7,9 +7,6 @@ import { Component, OnInit, Input } from '@angular/core';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit {
-  //TODO: pass in a user to pull user info
-  //TODO: figure out how to pull breadcrumb path from route
-  @Input() title;
 
   constructor(public userContext: UserContextService) { }
 
