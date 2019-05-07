438ada4f04f09439424d8ae11b6e2cff6e3dd64a
Ahmed Mahfouz <ahmed.mahfouz@cgifederal.com>
2019-05-07 00:15:42 -0400

    CEF-96:Modifying the properties structure and referencing

--- cef-web/src/main/java/gov/epa/cef/web/config/LocalSecurityConfig.java
+++ cef-web/src/main/java/gov/epa/cef/web/config/LocalSecurityConfig.java
@@ -2,14 +2,11 @@ package gov.epa.cef.web.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.ImportResource;
 import org.springframework.context.annotation.Profile;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
-import gov.epa.cdx.shared.security.naas.CdxHandoffPreAuthenticationFilter;
 import gov.epa.cef.web.security.AppRole;
 import gov.epa.cef.web.security.ApplicationSecurityUtils;
 import gov.epa.cef.web.security.mock.MockHandoffFilter;
@@ -17,8 +14,6 @@ import gov.epa.cef.web.security.mock.MockHandoffFilter;
 @Profile("dev")
 @Configuration
 @EnableWebSecurity
-@ComponentScan(basePackages = { "gov.epa.cdx.shared" })
-@ImportResource(locations = { "file:${spring.config.dir}/cdx-shared/cdx-shared-config.xml" })
 public class LocalSecurityConfig extends SecurityConfig {
 
 	@Autowired
@@ -30,7 +25,7 @@ public class LocalSecurityConfig extends SecurityConfig {
 			.antMatcher("/**")
 			.csrf().disable()
 			.addFilter(cdxWebPreAuthFilter())
-			.addFilterBefore(mockHandoffFilter(), CdxHandoffPreAuthenticationFilter.class)
+			.addFilterBefore(mockHandoffFilter(), MockHandoffFilter.class)
 			.authorizeRequests()
 			.antMatchers("/**")
 			.hasAnyRole(
