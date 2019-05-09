2888bdbbb3cc4d8903ceee20cf27235ec5cc5809
Ahmed Mahfouz <ahmed.mahfouz@cgifederal.com>
2019-05-09 17:54:41 -0400

    CEF-101:basic configuration and fixing angular unit tests

--- cef-web/src/main/webapp/src/app/facility-dashboard/facility-dashboard.component.spec.ts
+++ cef-web/src/main/webapp/src/app/facility-dashboard/facility-dashboard.component.spec.ts
@@ -1,14 +1,40 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { FacilityDashboardComponent } from './facility-dashboard.component';
+import { NotificationListComponent } from './notification-list/notification-list.component';
+import { FacilityListComponent} from './facility-list/facility-list.component';
+import { FacilityListItemComponent } from "src/app/facility-dashboard/facility-list/facility-list-item/facility-list-item.component";
+
+import { SharedModule } from "src/app/shared/shared.module";
+import { RouterTestingModule } from '@angular/router/testing';
+import { Facility } from "src/app/model/facility";
+
+import { HttpClientModule }    from '@angular/common/http';
 
 describe('FacilityDashboardComponent', () => {
   let component: FacilityDashboardComponent;
   let fixture: ComponentFixture<FacilityDashboardComponent>;
-
+  let facility = {
+      cdxFacilityId: 123,
+      epaRegistryId: "123",
+      programId: "123",
+      facilityName: "test-facility",
+      address: "123 elm st",
+      address2: "",
+      city: "Fairfax",
+      state: "Virginia",
+      county: "US",
+      zipCode: "22033"
+  };
+  let facilities: Facility[] = [facility];
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ FacilityDashboardComponent ]
+      declarations: [ FacilityDashboardComponent,
+                      NotificationListComponent,
+                      FacilityListComponent,
+                      FacilityListItemComponent
+                      ],
+      imports: [SharedModule, RouterTestingModule, HttpClientModule]
     })
     .compileComponents();
   }));
@@ -16,6 +42,7 @@ describe('FacilityDashboardComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(FacilityDashboardComponent);
     component = fixture.componentInstance;
+    component.facilities=facilities;
     fixture.detectChanges();
   });
 
--- cef-web/src/main/webapp/src/app/services/route-context.service.spec.ts
+++ cef-web/src/main/webapp/src/app/services/route-context.service.spec.ts
@@ -1,9 +1,12 @@
 import { TestBed } from '@angular/core/testing';
 
 import { RouteContextService } from './route-context.service';
+import { RouterTestingModule } from '@angular/router/testing';
 
 describe('RouteContextService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
+  beforeEach(() => TestBed.configureTestingModule({imports: [
+                                                             RouterTestingModule
+                                                             ]}));
 
   it('should be created', () => {
     const service: RouteContextService = TestBed.get(RouteContextService);
