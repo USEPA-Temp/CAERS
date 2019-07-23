5ddea419313c9ba0025ba420449a95bccd8d1932
Thomas Fesperman <thomas.fesperman@cgifederal.com>
2019-07-23 10:44:36 -0400

    [CEF-272] Adding Release Point to xml mapping

--- cef-web/src/main/java/gov/epa/cef/web/service/mapper/cers/CersReleasePointMapper.java
+++ cef-web/src/main/java/gov/epa/cef/web/service/mapper/cers/CersReleasePointMapper.java
@@ -8,10 +8,34 @@ import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 
 import gov.epa.cef.web.domain.ReleasePoint;
+import net.exchangenetwork.schema.cer._1._2.GeographicCoordinatesDataType;
 import net.exchangenetwork.schema.cer._1._2.IdentificationDataType;
+import net.exchangenetwork.schema.cer._1._2.ReleasePointDataType;
 
 @Mapper(componentModel = "spring", uses = {})
 public interface CersReleasePointMapper {
+
+    @Mapping(source="typeCode.code", target="releasePointTypeCode")
+    @Mapping(source="description", target="releasePointDescription")
+    @Mapping(source="stackHeight", target="releasePointStackHeightMeasure")
+    @Mapping(source="stackHeightUomCode.code", target="releasePointStackHeightUnitofMeasureCode")
+    @Mapping(source="stackDiameter", target="releasePointStackDiameterMeasure")
+    @Mapping(source="stackDiameterUomCode.code", target="releasePointStackDiameterUnitofMeasureCode")
+    @Mapping(source="exitGasVelocity", target="releasePointExitGasVelocityMeasure")
+    @Mapping(source="exitGasVelocityUomCode.code", target="releasePointExitGasVelocityUnitofMeasureCode")
+    @Mapping(source="exitGasFlowRate", target="releasePointExitGasFlowRateMeasure")
+    @Mapping(source="exitGasFlowUomCode.code", target="releasePointExitGasFlowRateUnitofMeasureCode")
+    @Mapping(source="exitGasTemperature", target="releasePointExitGasTemperatureMeasure")
+    @Mapping(source="comments", target="releasePointComment")
+    @Mapping(source="operatingStatusCode.code", target="releasePointStatusCode")
+    @Mapping(source="statusYear", target="releasePointStatusCodeYear")
+    @Mapping(source=".", target="releasePointIdentification")
+    @Mapping(source=".", target="releasePointGeographicCoordinates")
+    ReleasePointDataType fromReleasePoint(ReleasePoint source);
+
+    @Mapping(source="latitude", target="latitudeMeasure")
+    @Mapping(source="longitude", target="longitudeMeasure")
+    GeographicCoordinatesDataType coordinatesFromReleasePoint(ReleasePoint source);
 
     @BeanMapping(ignoreByDefault = true)
     @Mapping(source="releasePointIdentifier", target="identifier")
