f1cddcb9aba2becde66b58e16a8b233c318577ab
Jim Horner <james.horner@cgifederal.com>
2019-08-13 23:28:36 -0400

    adds Hazelcast jar(s) to pom adds caching to spring configuration creates a HazelcastInstance removes Hikari pooling in favor of Tomcat datasource pooling

--- cef-web/src/main/java/gov/epa/cef/web/CefWebApplication.java
+++ cef-web/src/main/java/gov/epa/cef/web/CefWebApplication.java
@@ -2,12 +2,14 @@ package gov.epa.cef.web;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.context.annotation.PropertySources;
 
 import gov.epa.cef.web.config.YamlPropertySourceFactory;
 
 @SpringBootApplication
+@EnableCaching
 @PropertySources({
     @PropertySource(factory = YamlPropertySourceFactory.class, value = "file:${spring.config.dir}/cef-web/cef-web-config.yml", ignoreResourceNotFound=true),
 })
--- cef-web/src/main/java/gov/epa/cef/web/domain/CalculationMethodCode.java
+++ cef-web/src/main/java/gov/epa/cef/web/domain/CalculationMethodCode.java
@@ -1,12 +1,17 @@
 package gov.epa.cef.web.domain;
 
+import gov.epa.cef.web.domain.common.BaseLookupEntity;
+import org.hibernate.annotations.Cache;
+import org.hibernate.annotations.CacheConcurrencyStrategy;
+
+import javax.persistence.Cacheable;
 import javax.persistence.Entity;
 import javax.persistence.Table;
 
-import gov.epa.cef.web.domain.common.BaseLookupEntity;
-
 @Entity
 @Table(name = "calculation_method_code")
+@Cacheable
+@Cache(usage = CacheConcurrencyStrategy.READ_ONLY)
 public class CalculationMethodCode extends BaseLookupEntity {
 
     private static final long serialVersionUID = 1L;
