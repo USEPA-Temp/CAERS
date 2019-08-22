e6b345698b2ff83854ffdfd7eda497a95ad8bccc
Jim Horner <james.horner@cgifederal.com>
2019-08-22 12:49:19 -0400

    revert? tests

--- cef-web/src/test/java/gov/epa/cef/web/service/impl/EmissionsProcessServiceImplTest.java
+++ cef-web/src/test/java/gov/epa/cef/web/service/impl/EmissionsProcessServiceImplTest.java
@@ -3,6 +3,7 @@ package gov.epa.cef.web.service.impl;
 import gov.epa.cef.web.domain.EmissionsProcess;
 import gov.epa.cef.web.repository.EmissionsProcessRepository;
 import gov.epa.cef.web.service.dto.EmissionsProcessDto;
+import gov.epa.cef.web.service.dto.EmissionsProcessSaveDto;
 import gov.epa.cef.web.service.mapper.EmissionsProcessMapper;
 import gov.epa.cef.web.service.mapper.ReleasePointApptMapper;
 import org.junit.Before;
@@ -24,6 +25,8 @@ import static org.mockito.Mockito.when;
 public class EmissionsProcessServiceImplTest extends BaseServiceTest {
 
     private EmissionsProcess emissionProcess;
+    private EmissionsProcessDto emissionsProcessDto;
+    private EmissionsProcessSaveDto emissionsProcessSaveDto;
 
     @Mock
     private EmissionsProcessRepository processRepo;
@@ -34,41 +37,53 @@ public class EmissionsProcessServiceImplTest extends BaseServiceTest {
     @Mock
     private ReleasePointApptMapper releasePointApptMapper;
 
-	@InjectMocks
-	private EmissionsProcessServiceImpl emissionsProcessServiceImpl;
+    @InjectMocks
+    private EmissionsProcessServiceImpl emissionsProcessServiceImpl;
 
-	@Before
-	public void init(){
+    @Before
+    public void init(){
 
-	    emissionProcess=new EmissionsProcess();
+        emissionProcess = new EmissionsProcess();
+        emissionProcess.setId(1L);
+        emissionsProcessDto = new EmissionsProcessDto();
+        emissionsProcessDto.setId(1L);
+        emissionsProcessSaveDto = new EmissionsProcessSaveDto();
+        emissionsProcessSaveDto.setId(1L);
         when(emissionsProcessMapper.emissionsProcessToEmissionsProcessDto(null)).thenReturn(null);
-        when(emissionsProcessMapper.emissionsProcessToEmissionsProcessDto(emissionProcess)).thenReturn(new EmissionsProcessDto());
+        when(emissionsProcessMapper.emissionsProcessToEmissionsProcessDto(emissionProcess)).thenReturn(emissionsProcessDto);
 
-	    when(processRepo.findById(1L)).thenReturn(Optional.of(emissionProcess));
-	    when(processRepo.findById(2L)).thenReturn(Optional.empty());
+        when(processRepo.findById(1L)).thenReturn(Optional.of(emissionProcess));
+        when(processRepo.findById(2L)).thenReturn(Optional.empty());
 
-	    List<EmissionsProcess> emissionsProcessList=new ArrayList<>();
-	    List<EmissionsProcessDto> emissionsProcessDtosList=new ArrayList<>();
+        List<EmissionsProcess> emissionsProcessList=new ArrayList<>();
+        List<EmissionsProcessDto> emissionsProcessDtosList=new ArrayList<>();
         when(emissionsProcessMapper.emissionsProcessesToEmissionsProcessDtos(emissionsProcessList))
                 .thenReturn(emissionsProcessDtosList);
         when(emissionsProcessMapper.emissionsProcessesToEmissionsProcessDtos(null)).thenReturn(null);
 
         when(processRepo.findByReleasePointApptsReleasePointId(1L)).thenReturn(emissionsProcessList);
-	    when(processRepo.findByReleasePointApptsReleasePointId(2L)).thenReturn(null);
+        when(processRepo.findByReleasePointApptsReleasePointId(2L)).thenReturn(null);
 
         when(processRepo.findByEmissionsUnitId(1L)).thenReturn(emissionsProcessList);
         when(processRepo.findByEmissionsUnitId(2L)).thenReturn(null);
 
+        when(processRepo.save(emissionProcess)).thenReturn(emissionProcess);
 
-	}
+    }
 
-	@Test
-	public void retrieveById_Should_ReturnEmissionProcessDtoObject_When_EmissionProcessExists(){
-	    EmissionsProcessDto emissionProcess=emissionsProcessServiceImpl.retrieveById(1L);
-	    assertNotEquals(null, emissionProcess);
-	}
+    @Test
+    public void update_Should_ReturnEmissionProcessDtoObject_When_EmissionProcessExists(){
+        EmissionsProcessDto emissionProcess = emissionsProcessServiceImpl.update(emissionsProcessSaveDto);
+        assertNotEquals(null, emissionProcess);
+    }
 
-	@Test
+    @Test
+    public void retrieveById_Should_ReturnEmissionProcessDtoObject_When_EmissionProcessExists(){
+        EmissionsProcessDto emissionProcess=emissionsProcessServiceImpl.retrieveById(1L);
+        assertNotEquals(null, emissionProcess);
+    }
+
+    @Test
     public void retrieveById_Should_ReturnNull_When_EmissionProcessNotExists(){
        EmissionsProcessDto emissionProcess=emissionsProcessServiceImpl.retrieveById(2L);
        assertEquals(null, emissionProcess);
