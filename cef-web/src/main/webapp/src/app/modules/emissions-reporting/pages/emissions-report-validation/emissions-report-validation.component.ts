1bf543e5ac4fbf48bbe8240b124c835bb67b97a0
blake fleming <blake.fleming@cgifederal.com>
2019-10-31 13:42:07 -0500

    added validationStatus enum

--- cef-web/src/main/webapp/src/app/modules/emissions-reporting/pages/emissions-report-validation/emissions-report-validation.component.ts
+++ cef-web/src/main/webapp/src/app/modules/emissions-reporting/pages/emissions-report-validation/emissions-report-validation.component.ts
@@ -4,6 +4,8 @@ import {EmissionsReportingService} from "src/app/core/services/emissions-reporti
 import {ActivatedRoute} from "@angular/router";
 import {FacilitySite} from "../../../../shared/models/facility-site";
 import {SharedService} from "../../../../core/services/shared.service";
+import { ValidationStatus } from 'src/app/shared/enums/validation-status.enum';
+
 
 @Component({
   selector: 'app-emissions-report-validation',
@@ -31,20 +33,20 @@ export class EmissionsReportValidationComponent implements OnInit {
         
           this.emissionsReportingService.validateReport(data.facilitySite.emissionsReport.id)
               .subscribe(validationResult => {
-                
+
                   this.validationResult = validationResult;
                   setTimeout(() => {
                     this.validationComplete = true;
                     if(validationResult['valid']){
                       if(this.hasWarnings()){
-                        data.facilitySite.emissionsReport.validationStatus = "PASSED_WARNINGS";
+                        data.facilitySite.emissionsReport.validationStatus = ValidationStatus.PASSED_WARNINGS;
                       }
                       else{
-                        data.facilitySite.emissionsReport.validationStatus = "PASSED";
+                        data.facilitySite.emissionsReport.validationStatus = ValidationStatus.PASSED;
                       }
                      }
                     else{
-                      data.facilitySite.emissionsReport.validationStatus = "FAILED";
+                      data.facilitySite.emissionsReport.validationStatus = ValidationStatus.FAILED;
                     }
                     this.sharedService.emitChange(data.facilitySite);
                   }, 5000);
---
+++ cef-web/src/main/webapp/src/app/shared/enums/validation-status.enum.ts
@@ -0,0 +1,5 @@
+export enum ValidationStatus {
+  PASSED = 'PASSED',
+  PASSED_WARNINGS = 'PASSED_WARNINGS',
+  FAILED = 'FAILED'
+}
