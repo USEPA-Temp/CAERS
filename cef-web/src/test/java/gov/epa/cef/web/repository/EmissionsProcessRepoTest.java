60cb6c6dcf944aa36e2e5d534c2fe5310bedda29
Ed Canoles <edward.canoles@cgifederal.com>
2019-11-04 09:59:24 -0500

    --Added updated package-lock.json to fix dependency issues for Angular modules (missing Toastr and Angular animations) --Added new baseTestData.sql for embedded postgreSQL unit tests to preload reusable data --Added unit test for deleting emission process to make sure that the emission process can be deleted from the database and that any child emissions are also deleted

---
+++ cef-web/src/test/java/gov/epa/cef/web/repository/EmissionsProcessRepoTest.java
@@ -0,0 +1,73 @@
+package gov.epa.cef.web.repository;
+
+import static org.junit.Assert.assertEquals;
+
+import java.util.Optional;
+
+import javax.sql.DataSource;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.jdbc.Sql;
+import org.springframework.test.context.jdbc.SqlGroup;
+
+import gov.epa.cef.web.config.CommonInitializers;
+import gov.epa.cef.web.domain.Emission;
+import gov.epa.cef.web.domain.EmissionsProcess;
+
+@SqlGroup(value = {@Sql("classpath:db/test/baseTestData.sql")})
+@ContextConfiguration(initializers = {
+    CommonInitializers.NoCacheInitializer.class
+})
+public class EmissionsProcessRepoTest extends BaseRepositoryTest {
+    
+    @Autowired
+    DataSource dataSource;
+
+    NamedParameterJdbcTemplate jdbcTemplate;
+
+    @Autowired
+    EmissionsProcessRepository processRepo;
+    
+    @Autowired
+    EmissionRepository emissionRepo;
+
+    @Before
+    public void _onJunitBeginTest() {
+
+        runWithMockUser();
+
+        //this.jdbcTemplate = new NamedParameterJdbcTemplate(this.dataSource);
+    }
+    
+    /**
+     * Verify that deleting an emission process works and that any child emissions 
+     * are also deleted
+     * @throws Exception
+     */
+    @Test
+    public void deletingProcess_should_DeleteChildEmissions() throws Exception {
+        
+        //verify the process and emissions exist
+        Optional<EmissionsProcess> process = processRepo.findById(9999991L);
+        assertEquals(true, process.isPresent());
+        
+        Optional<Emission> emission = emissionRepo.findById(9999991L);
+        assertEquals(true, emission.isPresent());
+        
+        //delete the process and verify that the emissions are gone as well
+        processRepo.deleteById(9999991L);
+        
+        //verify the process and emissions exist
+        process = processRepo.findById(9999991L);
+        assertEquals(false, process.isPresent());
+        
+        emission = emissionRepo.findById(9999991L);
+        assertEquals(false, emission.isPresent());
+        
+    }
+       
+}
