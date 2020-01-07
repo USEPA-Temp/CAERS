d3bc4f53a61187ae762adf426c9fde46476b8e99
Anne Lee <anne.lee1@cgifederal.com>
2020-01-07 17:42:42 -0500

    -- CEF-515 Added QA Check: Facility must have an Emissions Inventory Contact Added QA check and message. Message shows up in QA results with link to Facility Information page. Updated unit test.

--- cef-web/src/test/java/gov/epa/cef/web/service/validation/validator/FacilitySiteValidatorTest.java
+++ cef-web/src/test/java/gov/epa/cef/web/service/validation/validator/FacilitySiteValidatorTest.java
@@ -4,6 +4,7 @@ package gov.epa.cef.web.service.validation.validator;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
@@ -14,7 +15,10 @@ import org.mockito.junit.MockitoJUnitRunner;
 
 import com.baidu.unbiz.fluentvalidator.ValidationError;
 
+import gov.epa.cef.web.domain.ContactTypeCode;
 import gov.epa.cef.web.domain.FacilitySite;
+import gov.epa.cef.web.domain.FacilitySiteContact;
+import gov.epa.cef.web.domain.FipsStateCode;
 import gov.epa.cef.web.service.validation.CefValidatorContext;
 import gov.epa.cef.web.service.validation.ValidationField;
 import gov.epa.cef.web.service.validation.validator.federal.FacilitySiteValidator;
@@ -41,18 +45,53 @@ public class FacilitySiteValidatorTest extends BaseValidatorTest {
         CefValidatorContext cefContext = createContext();
         FacilitySite testData = createBaseFacilitySite();
         testData.setEisProgramId(null);
+        testData.setContacts(null);
 
         assertFalse(this.validator.validate(cefContext, testData));
-        assertTrue(cefContext.result.getErrors() != null && cefContext.result.getErrors().size() == 1);
+        assertTrue(cefContext.result.getErrors() != null && cefContext.result.getErrors().size() == 2);
 
         Map<String, List<ValidationError>> errorMap = mapErrors(cefContext.result.getErrors());
         assertTrue(errorMap.containsKey(ValidationField.FACILITY_EIS_ID.value()) && errorMap.get(ValidationField.FACILITY_EIS_ID.value()).size() == 1);
+        assertTrue(errorMap.containsKey(ValidationField.FACILITY_CONTACT.value()) && errorMap.get(ValidationField.FACILITY_CONTACT.value()).size() == 1);
     }
 
     private FacilitySite createBaseFacilitySite() {
 
         FacilitySite result = new FacilitySite();
         result.setEisProgramId("1");
+        
+        ContactTypeCode contactTypeCode = new ContactTypeCode();
+        contactTypeCode.setCode("FAC");
+        
+        FipsStateCode stateCode = new FipsStateCode();
+        stateCode.setCode("13");
+        stateCode.setUspsCode("GA");
+        
+        FipsStateCode mailingStateCode = new FipsStateCode();
+        mailingStateCode.setCode("13");     
+        mailingStateCode.setUspsCode("GA");
+        
+        List<FacilitySiteContact> contactList = new ArrayList<FacilitySiteContact>();
+        FacilitySiteContact contact = new FacilitySiteContact();
+        
+        contact.setType(contactTypeCode);
+        contact.setFirstName("Jane");
+        contact.setLastName("Doe");
+        contact.setEmail("jane.doe@test.com");
+        contact.setPhone("1234567890");
+        contact.setPhoneExt("");
+        contact.setStreetAddress("123 Test Street");
+        contact.setCity("Fitzgerald");
+        contact.setStateCode(stateCode);
+        contact.setPostalCode("31750");
+        contact.setCounty("Whitfield");
+        contact.setMailingStreetAddress("123 Test Street");
+        contact.setMailingCity("Fitzgerald");
+        contact.setMailingStateCode(mailingStateCode);
+        contact.setMailingPostalCode("31750");
+        contactList.add(contact);
+        
+        result.setContacts(contactList);
 
         return result;
     }
