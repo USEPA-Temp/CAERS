a78f51141970532b56e774b20be7768507516360
Anne Lee <anne.lee1@cgifederal.com>
2020-01-30 09:00:26 -0500

    -- CEF-541 Added QA checks for emissions unit -- CEF-541, CEF-542 QA check for required status year if status is not OP, and QA for status year range -- CEF-543 QA check for design capacity warning for specified unit types -- CEF-544 QA check for required Uom to submit design capacity -- CEF-559 QA check for design capacity range Added unit tests and updated edit emissions unit info panel validations

--- cef-web/src/main/java/gov/epa/cef/web/service/validation/validator/federal/EmissionsUnitValidator.java
+++ cef-web/src/main/java/gov/epa/cef/web/service/validation/validator/federal/EmissionsUnitValidator.java
@@ -1,8 +1,18 @@
 package gov.epa.cef.web.service.validation.validator.federal;
 
 import gov.epa.cef.web.domain.EmissionsUnit;
+import gov.epa.cef.web.service.dto.EntityType;
+import gov.epa.cef.web.service.dto.ValidationDetailDto;
+import gov.epa.cef.web.service.validation.CefValidatorContext;
+import gov.epa.cef.web.service.validation.ValidationField;
 import gov.epa.cef.web.service.validation.ValidationRegistry;
 import gov.epa.cef.web.service.validation.validator.BaseValidator;
+
+import java.text.MessageFormat;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 import org.springframework.stereotype.Component;
 
 import com.baidu.unbiz.fluentvalidator.FluentValidator;
@@ -22,4 +32,79 @@ public class EmissionsUnitValidator extends BaseValidator<EmissionsUnit> {
         validator.onEach(emissionsUnit.getEmissionsProcesses(),
             registry.findOneByType(EmissionsProcessValidator.class));
     }
+    
+    @Override
+    public boolean validate(ValidatorContext validatorContext, EmissionsUnit emissionsUnit) {
+
+        boolean result = true;
+
+        CefValidatorContext context = getCefValidatorContext(validatorContext);
+        
+        // If unit operation status is not operating, status year is required
+        if (!"OP".contentEquals(emissionsUnit.getOperatingStatusCode().getCode()) && emissionsUnit.getStatusYear() == null) {
+ 	
+        	result = false;
+        	context.addFederalError(
+        			ValidationField.EMISSIONS_UNIT_STATUS.value(), "emissionsUnit.status.required",
+        			createValidationDetails(emissionsUnit));
+        }
+        
+        // Status year must be between 1900 and 2050
+        if (emissionsUnit.getStatusYear() != null && (emissionsUnit.getStatusYear() < 1900 || emissionsUnit.getStatusYear() > 2050)) {
+        	
+        	result = false;
+        	context.addFederalError(
+        			ValidationField.EMISSIONS_UNIT_STATUS.value(), "emissionsUnit.status.range",
+        			createValidationDetails(emissionsUnit));
+        }
+        
+        // Design capacity warning
+        if (emissionsUnit.getUnitTypeCode() != null && emissionsUnit.getDesignCapacity() == null) {
+        	List<String> typeCodeWarn = new ArrayList<String>(); 
+        	Collections.addAll(typeCodeWarn, "100", "120", "140", "160", "180");
+        	
+        	for (String code: typeCodeWarn) {
+          	if (code.contentEquals(emissionsUnit.getUnitTypeCode().getCode())) {
+          		
+          		result = false;
+          		context.addFederalWarning(
+          				ValidationField.EMISSIONS_UNIT_CAPACITY.value(), "emissionsUnit.capacity.check",
+          				createValidationDetails(emissionsUnit),
+          				emissionsUnit.getUnitTypeCode().getDescription());
+          	}
+          };
+        }
+        
+        // Design capacity range
+        if ((emissionsUnit.getDesignCapacity() != null)
+        	&& (emissionsUnit.getDesignCapacity().doubleValue() < 0.01 || emissionsUnit.getDesignCapacity().doubleValue() > 100000000)) {
+        	
+        	result = false;
+        	context.addFederalError(
+        			ValidationField.EMISSIONS_UNIT_CAPACITY.value(),
+        			"emissionsUnit.capacity.range",
+        			createValidationDetails(emissionsUnit));
+        } 
+        
+        // Design capacity and UoM must be reported together.
+        if ((emissionsUnit.getDesignCapacity() != null && emissionsUnit.getUnitOfMeasureCode() == null)
+        	|| (emissionsUnit.getDesignCapacity() == null && emissionsUnit.getUnitOfMeasureCode() != null)) {
+        	
+        	result = false;
+        	context.addFederalError(
+        			ValidationField.EMISSIONS_UNIT_CAPACITY.value(),
+        			"emissionsUnit.capacity.required",
+        			createValidationDetails(emissionsUnit));
+        }
+        
+        return result;
+    }
+    
+    private ValidationDetailDto createValidationDetails(EmissionsUnit source) {
+
+      String description = MessageFormat.format("Emissions Unit: {0}", source.getUnitIdentifier());
+
+      ValidationDetailDto dto = new ValidationDetailDto(source.getId(), source.getUnitIdentifier(), EntityType.EMISSIONS_UNIT, description);
+      return dto;
+  }
 }
--- cef-web/src/test/java/gov/epa/cef/web/service/validation/validator/EmissionsUnitValidatorTest.java
+++ cef-web/src/test/java/gov/epa/cef/web/service/validation/validator/EmissionsUnitValidatorTest.java
@@ -1,15 +1,26 @@
 package gov.epa.cef.web.service.validation.validator;
 
 
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
+
+import java.math.BigDecimal;
+import java.util.List;
+import java.util.Map;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.junit.MockitoJUnitRunner;
 
+import com.baidu.unbiz.fluentvalidator.ValidationError;
+
 import gov.epa.cef.web.domain.EmissionsUnit;
+import gov.epa.cef.web.domain.OperatingStatusCode;
+import gov.epa.cef.web.domain.UnitMeasureCode;
+import gov.epa.cef.web.domain.UnitTypeCode;
 import gov.epa.cef.web.service.validation.CefValidatorContext;
+import gov.epa.cef.web.service.validation.ValidationField;
 import gov.epa.cef.web.service.validation.validator.federal.EmissionsUnitValidator;
 
 @RunWith(MockitoJUnitRunner.Silent.class)
@@ -27,12 +38,152 @@ public class EmissionsUnitValidatorTest extends BaseValidatorTest {
         assertTrue(this.validator.validate(cefContext, testData));
         assertTrue(cefContext.result.getErrors() == null || cefContext.result.getErrors().isEmpty());
     }
+    
+    @Test
+    public void simpleValidateOperationStatusTypeFailTest() {
+
+        CefValidatorContext cefContext = createContext();
+        EmissionsUnit testData = createBaseEmissionsUnit();
+        
+        OperatingStatusCode opStatusCode = new OperatingStatusCode();
+        opStatusCode.setCode("TS");
+        testData.setOperatingStatusCode(opStatusCode);
+        testData.setStatusYear(null);
+        
+        assertFalse(this.validator.validate(cefContext, testData));
+        assertTrue(cefContext.result.getErrors() != null && cefContext.result.getErrors().size() == 1);
+        
+        Map<String, List<ValidationError>> errorMap = mapErrors(cefContext.result.getErrors());
+        assertTrue(errorMap.containsKey(ValidationField.EMISSIONS_UNIT_STATUS.value()) && errorMap.get(ValidationField.EMISSIONS_UNIT_STATUS.value()).size() == 1);
+    }
+    
+    @Test
+    public void simpleValidateStatusYearRangeTest() {
+
+        CefValidatorContext cefContext = createContext();
+        EmissionsUnit testData = createBaseEmissionsUnit();
+        
+        testData.setStatusYear((short) 1900);
+        
+        assertTrue(this.validator.validate(cefContext, testData));
+        assertTrue(cefContext.result.getErrors() == null || cefContext.result.getErrors().isEmpty());
+        
+        cefContext = createContext();
+        testData.setStatusYear((short) 2050);
+        
+        assertTrue(this.validator.validate(cefContext, testData));
+        assertTrue(cefContext.result.getErrors() == null || cefContext.result.getErrors().isEmpty());
+    }
+    
+    @Test
+    public void simpleValidateStatusYearRangeFailTest() {
+	      
+        CefValidatorContext cefContext = createContext();
+        EmissionsUnit testData = createBaseEmissionsUnit();
+        
+        testData.setStatusYear((short) 1800);
+        
+        assertFalse(this.validator.validate(cefContext, testData));
+        assertTrue(cefContext.result.getErrors() != null && cefContext.result.getErrors().size() == 1);
+        
+        Map<String, List<ValidationError>> errorMap = mapErrors(cefContext.result.getErrors());
+        assertTrue(errorMap.containsKey(ValidationField.EMISSIONS_UNIT_STATUS.value()) && errorMap.get(ValidationField.EMISSIONS_UNIT_STATUS.value()).size() == 1);
+        
+        cefContext = createContext();
+        testData.setStatusYear((short) 2051);
+        
+        assertFalse(this.validator.validate(cefContext, testData));
+        assertTrue(cefContext.result.getErrors() != null && cefContext.result.getErrors().size() == 1);
+        
+        errorMap = mapErrors(cefContext.result.getErrors());
+        assertTrue(errorMap.containsKey(ValidationField.EMISSIONS_UNIT_STATUS.value()) && errorMap.get(ValidationField.EMISSIONS_UNIT_STATUS.value()).size() == 1);
+    }
+    
+    @Test
+    public void designCapacityCheckCalculationFailTest() {
+
+        CefValidatorContext cefContext = createContext();
+        EmissionsUnit testData = createBaseEmissionsUnit();
+
+        cefContext = createContext();
+        testData.setDesignCapacity(null);
+        testData.setUnitOfMeasureCode(null);
+        
+        assertFalse(this.validator.validate(cefContext, testData));
+        assertTrue(cefContext.result.getErrors() != null && cefContext.result.getErrors().size() == 1);
+
+        Map<String, List<ValidationError>> errorMap = mapErrors(cefContext.result.getErrors());
+        assertTrue(errorMap.containsKey(ValidationField.EMISSIONS_UNIT_CAPACITY.value()) && errorMap.get(ValidationField.EMISSIONS_UNIT_CAPACITY.value()).size() == 1);
+    }
+    
+    @Test
+    public void designCapacityFailTest() {
+    	
+    	CefValidatorContext cefContext = createContext();
+    	EmissionsUnit testData = createBaseEmissionsUnit();
+    	testData.setDesignCapacity(BigDecimal.valueOf(0.0001));
+    	
+    	assertFalse(this.validator.validate(cefContext, testData));
+    	assertTrue(cefContext.result.getErrors() != null || cefContext.result.getErrors().size() == 1);
+    	
+    	Map<String, List<ValidationError>> errorMap = mapErrors(cefContext.result.getErrors());
+    	assertTrue(errorMap.containsKey(ValidationField.EMISSIONS_UNIT_CAPACITY.value()) && errorMap.get(ValidationField.EMISSIONS_UNIT_CAPACITY.value()).size() == 1);
+    	
+    	cefContext = createContext();
+    	testData.setDesignCapacity(BigDecimal.valueOf(200000000));
+    	
+    	assertFalse(this.validator.validate(cefContext, testData));
+    	assertTrue(cefContext.result.getErrors() != null || cefContext.result.getErrors().size() == 1);
+    }
+    
+    @Test
+    public void designCapacityAndUomRequiredFailTest() {
+
+        CefValidatorContext cefContext = createContext();
+        EmissionsUnit testData = createBaseEmissionsUnit();
+        UnitTypeCode utc = new UnitTypeCode();
+        utc.setCode("200");
+        testData.setUnitTypeCode(utc);
+        testData.setDesignCapacity(null);
+        
+        assertFalse(this.validator.validate(cefContext, testData));
+        assertTrue(cefContext.result.getErrors() != null && cefContext.result.getErrors().size() == 1);
+
+        Map<String, List<ValidationError>> errorMap = mapErrors(cefContext.result.getErrors());
+        assertTrue(errorMap.containsKey(ValidationField.EMISSIONS_UNIT_CAPACITY.value()) && errorMap.get(ValidationField.EMISSIONS_UNIT_CAPACITY.value()).size() == 1);
+        
+        cefContext = createContext();
+        testData.setUnitOfMeasureCode(null);
+        testData.setDesignCapacity(BigDecimal.valueOf(1000.0));
+        
+        assertFalse(this.validator.validate(cefContext, testData));
+        assertTrue(cefContext.result.getErrors() != null && cefContext.result.getErrors().size() == 1);
+
+        errorMap = mapErrors(cefContext.result.getErrors());
+        assertTrue(errorMap.containsKey(ValidationField.EMISSIONS_UNIT_CAPACITY.value()) && errorMap.get(ValidationField.EMISSIONS_UNIT_CAPACITY.value()).size() == 1);
+    }
+    
+    
 
     private EmissionsUnit createBaseEmissionsUnit() {
 
-        // No tests for Emissions Units yet
         EmissionsUnit result = new EmissionsUnit();
 
+        OperatingStatusCode opStatCode = new OperatingStatusCode();
+        opStatCode.setCode("OP");
+        
+        UnitTypeCode utc = new UnitTypeCode();
+        utc.setCode("100");
+        
+        UnitMeasureCode capUom = new UnitMeasureCode();
+        capUom.setCode("CURIE");
+        
+        result.setStatusYear((short) 2000);
+        result.setOperatingStatusCode(opStatCode);
+        result.setUnitTypeCode(utc);
+        result.setUnitOfMeasureCode(capUom);
+        result.setDesignCapacity(BigDecimal.valueOf(100));
+        
         return result;
     }
 }
