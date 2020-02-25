44b5e6a620e110e15e85292093f0c6ef43a1b6dc
blake fleming <blake.fleming@cgifederal.com>
2020-02-25 11:10:17 -0600

    added a check if mailingPostal or postalCode fields are empty then they are set to null so the QA check doesnt throw false errors

--- cef-web/src/main/webapp/src/app/modules/emissions-reporting/pages/edit-facility-contact/edit-facility-contact.component.ts
+++ cef-web/src/main/webapp/src/app/modules/emissions-reporting/pages/edit-facility-contact/edit-facility-contact.component.ts
@@ -137,6 +137,13 @@ export class EditFacilityContactComponent implements OnInit {
       const saveContact = new FacilitySiteContact();
       Object.assign(saveContact, this.contactForm.value);
 
+      // if values are empty set to null to avoid QA check throwing false errors
+      if (saveContact.mailingPostalCode === '') {
+        saveContact.mailingPostalCode = null;
+      }
+      if (saveContact.postalCode === '') {
+        saveContact.postalCode = null;
+      }
       if (this.createMode) {
 
         saveContact.facilitySiteId = this.facilitySite.id;
@@ -150,7 +157,6 @@ export class EditFacilityContactComponent implements OnInit {
       } else {
 
         saveContact.id = this.facilityContact.id;
-
         this.contactService.update(saveContact)
         .subscribe(() => {
 
