aae2efa985dfaeab7a73e8e893f61db224514c75
blake fleming <blake.fleming@cgifederal.com>
2020-03-05 10:10:31 -0600

    moved url declaration outside of if statement

--- cef-web/src/main/webapp/src/app/core/services/report-download.service.ts
+++ cef-web/src/main/webapp/src/app/core/services/report-download.service.ts
@@ -13,11 +13,12 @@ export class ReportDownloadService {
         'emissionsNumeratorUom','emissionsDenominatorUom','emissionsFactorText','emissionsComment','lastModifiedBy','lastModifiedDate']);
         const blob = new Blob(['\ufeff' + csvData], { type: 'text/csv;charset=utf-8;' });
         const dwldLink = document.createElement('a');
+        var url;
         if (window.navigator && window.navigator.msSaveOrOpenBlob) {
           window.navigator.msSaveOrOpenBlob(blob, filename + '.csv');
           return;
         } else {
-          var url   = URL.createObjectURL(blob);
+          url = URL.createObjectURL(blob);
         }
 
         const isSafariBrowser = navigator.userAgent.indexOf('Safari') !== -1 && navigator.userAgent.indexOf('Chrome') === -1;
