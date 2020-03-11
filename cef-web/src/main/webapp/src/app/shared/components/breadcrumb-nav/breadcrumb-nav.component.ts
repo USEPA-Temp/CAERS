530c0b48f4e5ca5e98d4fff339c20b25310b6b1b
blake fleming <blake.fleming@cgifederal.com>
2020-03-11 14:59:14 -0500

    added a null check on the user.role in the html to avoid erroring causing the My Facilities link to disappear

--- cef-web/src/main/webapp/src/app/shared/components/breadcrumb-nav/breadcrumb-nav.component.ts
+++ cef-web/src/main/webapp/src/app/shared/components/breadcrumb-nav/breadcrumb-nav.component.ts
@@ -21,7 +21,7 @@ export class BreadcrumbNavComponent implements OnInit {
     public breadcrumbs: BreadCrumb[];
     year: number;
     baseLabel: string;
-    user:User = new User();
+    user: User;
     url: string;
 
     constructor( private router: Router, private activeRoute: ActivatedRoute,
