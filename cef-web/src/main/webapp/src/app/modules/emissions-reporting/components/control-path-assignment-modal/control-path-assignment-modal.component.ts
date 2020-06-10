ef27fc36f87e03a2eac73976ebf213dd6bcf3b44
Anne Lee <anne.lee1@cgifederal.com>
2020-06-10 10:55:52 -0400

    CEF-968 updated for edit control device front end validtion error

--- cef-web/src/main/webapp/src/app/modules/emissions-reporting/components/control-path-assignment-modal/control-path-assignment-modal.component.ts
+++ cef-web/src/main/webapp/src/app/modules/emissions-reporting/components/control-path-assignment-modal/control-path-assignment-modal.component.ts
@@ -151,8 +151,8 @@ export class ControlPathAssignmentModalComponent implements OnInit {
 
     // if control is chosen, check all control assignments with selected control
     if (this.controlPathAssignmentForm.get('control').value !== null) {
-        controlList = this.controlAssignmentList.filter(val => val.control !== null
-        && (val.control.id === this.controlPathAssignmentForm.get('control').value.id)).map((item) => item.id);
+        controlList = [...new Set(this.controlAssignmentList.filter(val => val.control !== null
+        && (val.control.id === this.controlPathAssignmentForm.get('control').value.id)).map(item => item.id))];
 
         if (this.edit && controlList && (this.selectedControlPathAssignment.control
         && this.selectedControlPathAssignment.control.id === this.controlPathAssignmentForm.get('control').value.id)) {
