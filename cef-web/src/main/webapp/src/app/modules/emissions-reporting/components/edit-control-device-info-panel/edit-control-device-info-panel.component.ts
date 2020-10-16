fc925a131e388c43e3571567dbc6e2c6cd5b9ac1
Brundage, Kevin (EPA) <brundage.kevin@epa.gov>
2020-10-16 17:01:46 -0400

    CEF-1080 allow control effectiveness to be between 1 and 100

--- cef-web/src/main/webapp/src/app/modules/emissions-reporting/components/edit-control-device-info-panel/edit-control-device-info-panel.component.ts
+++ cef-web/src/main/webapp/src/app/modules/emissions-reporting/components/edit-control-device-info-panel/edit-control-device-info-panel.component.ts
@@ -31,7 +31,7 @@ export class EditControlDeviceInfoPanelComponent implements OnInit, OnChanges {
     ]],
     percentControl: ['', [
       Validators.max(100.0),
-      Validators.min(5),
+      Validators.min(1),
       Validators.pattern('^[0-9]{1,3}([\.][0-9]{1})?$')
     ]],
     operatingStatusCode: [null, Validators.required],
