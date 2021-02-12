215677191de9cc9db5e224219fb95bace95e1830
sahebert <samuel.hebert@cgifederal.com>
2021-02-12 10:47:15 -0600

    💄 CEF-1192 | Updating UI Text
    * Added a setTimeout() to hide the informational blue text after 6 seconds upon triggering

--- cef-web/src/main/webapp/src/app/modules/emissions-reporting/components/edit-process-reporting-period-panel/edit-process-reporting-period-panel.component.ts
+++ cef-web/src/main/webapp/src/app/modules/emissions-reporting/components/edit-process-reporting-period-panel/edit-process-reporting-period-panel.component.ts
@@ -1,198 +1,204 @@
-import { Component, OnInit, OnChanges, Input } from '@angular/core';
-import { FormBuilder, Validators, ValidatorFn, FormGroup, ValidationErrors } from '@angular/forms';
-import { LookupService } from 'src/app/core/services/lookup.service';
-import { BaseCodeLookup } from 'src/app/shared/models/base-code-lookup';
-import { ReportingPeriod } from 'src/app/shared/models/reporting-period';
-import { FormUtilsService } from 'src/app/core/services/form-utils.service';
-import { UnitMeasureCode } from 'src/app/shared/models/unit-measure-code';
-import { legacyUomValidator } from 'src/app/modules/shared/directives/legacy-uom-validator.directive';
-import { SharedService } from 'src/app/core/services/shared.service';
+import {Component, OnInit, OnChanges, Input} from '@angular/core';
+import {FormBuilder, Validators, ValidatorFn, FormGroup, ValidationErrors} from '@angular/forms';
+import {LookupService} from 'src/app/core/services/lookup.service';
+import {BaseCodeLookup} from 'src/app/shared/models/base-code-lookup';
+import {ReportingPeriod} from 'src/app/shared/models/reporting-period';
+import {FormUtilsService} from 'src/app/core/services/form-utils.service';
+import {UnitMeasureCode} from 'src/app/shared/models/unit-measure-code';
+import {legacyUomValidator} from 'src/app/modules/shared/directives/legacy-uom-validator.directive';
+import {SharedService} from 'src/app/core/services/shared.service';
 
 @Component({
-  selector: 'app-edit-process-reporting-period-panel',
-  templateUrl: './edit-process-reporting-period-panel.component.html',
-  styleUrls: ['./edit-process-reporting-period-panel.component.scss']
+    selector: 'app-edit-process-reporting-period-panel',
+    templateUrl: './edit-process-reporting-period-panel.component.html',
+    styleUrls: ['./edit-process-reporting-period-panel.component.scss']
 })
 export class EditProcessReportingPeriodPanelComponent implements OnInit, OnChanges {
-  @Input() reportingPeriod: ReportingPeriod;
-  @Input() sccCode: string;
-  isFuelUseScc = false;
-  reportingPeriodForm = this.fb.group({
-    reportingPeriodTypeCode: [{code: 'A'}, Validators.required],
-    emissionsOperatingTypeCode: [null, Validators.required],
-    calculationParameterTypeCode: [null, Validators.required],
-    calculationParameterValue: ['', [
-      Validators.required,
-      Validators.min(0),
-      Validators.pattern('^[0-9]*\\.?[0-9]+$')
-    ]],
-    calculationParameterUom: [null, [Validators.required, legacyUomValidator()]],
-    calculationMaterialCode: [null, Validators.required],
-    fuelUseValue: ['', [
-      Validators.min(0),
-      Validators.pattern('^[0-9]*\\.?[0-9]+$')
-    ]],
-    fuelUseUom: [null, [legacyUomValidator()]],
-    fuelUseMaterialCode: [null],
-    heatContentValue: ['', [
-      Validators.min(0),
-      Validators.pattern('^[0-9]*\\.?[0-9]+$')
-    ]],
-    heatContentUom: [null, [legacyUomValidator()]],
-    comments: [null, Validators.maxLength(400)]
-  }, { validators: [
-    this.checkFuelUseFields(),
-    this.checkHeatCapacityUom()
-  ]});
-
-  materialValues: BaseCodeLookup[];
-  fuelUseMaterialValues: BaseCodeLookup[];
-  parameterTypeValues: BaseCodeLookup[];
-  operatingStatusValues: BaseCodeLookup[];
-  reportingPeriodValues: BaseCodeLookup[] = [];
-  uomValues: UnitMeasureCode[];
-  denominatorUomValues: UnitMeasureCode[];
-  fuelUseUomValues: UnitMeasureCode[];
-  heatContentUomValues: UnitMeasureCode[];
-  showFuelDataCopyMessage = false;
-  sccFuelUsefieldsWarning = null;
-
-  constructor(
-    private lookupService: LookupService,
-    public formUtils: FormUtilsService,
-    private sharedService: SharedService,
-    private fb: FormBuilder) { }
-
-  ngOnInit() {
-
-    this.lookupService.retrieveCalcMaterial()
-    .subscribe(result => {
-      this.materialValues = result;
+    @Input() reportingPeriod: ReportingPeriod;
+    @Input() sccCode: string;
+    isFuelUseScc = false;
+    reportingPeriodForm = this.fb.group({
+        reportingPeriodTypeCode: [{code: 'A'}, Validators.required],
+        emissionsOperatingTypeCode: [null, Validators.required],
+        calculationParameterTypeCode: [null, Validators.required],
+        calculationParameterValue: ['', [
+            Validators.required,
+            Validators.min(0),
+            Validators.pattern('^[0-9]*\\.?[0-9]+$')
+        ]],
+        calculationParameterUom: [null, [Validators.required, legacyUomValidator()]],
+        calculationMaterialCode: [null, Validators.required],
+        fuelUseValue: ['', [
+            Validators.min(0),
+            Validators.pattern('^[0-9]*\\.?[0-9]+$')
+        ]],
+        fuelUseUom: [null, [legacyUomValidator()]],
+        fuelUseMaterialCode: [null],
+        heatContentValue: ['', [
+            Validators.min(0),
+            Validators.pattern('^[0-9]*\\.?[0-9]+$')
+        ]],
+        heatContentUom: [null, [legacyUomValidator()]],
+        comments: [null, Validators.maxLength(400)]
+    }, {
+        validators: [
+            this.checkFuelUseFields(),
+            this.checkHeatCapacityUom()
+        ]
     });
 
-    this.lookupService.retrieveFuelUseMaterial()
-    .subscribe(result => {
-      this.fuelUseMaterialValues = result;
-    });
+    materialValues: BaseCodeLookup[];
+    fuelUseMaterialValues: BaseCodeLookup[];
+    parameterTypeValues: BaseCodeLookup[];
+    operatingStatusValues: BaseCodeLookup[];
+    reportingPeriodValues: BaseCodeLookup[] = [];
+    uomValues: UnitMeasureCode[];
+    denominatorUomValues: UnitMeasureCode[];
+    fuelUseUomValues: UnitMeasureCode[];
+    heatContentUomValues: UnitMeasureCode[];
+    showFuelDataCopyMessage = false;
+    sccFuelUsefieldsWarning = null;
 
-    this.lookupService.retrieveCalcParam()
-    .subscribe(result => {
-      this.parameterTypeValues = result;
-    });
-
-    this.lookupService.retrieveEmissionsOperatingType()
-    .subscribe(result => {
-      this.operatingStatusValues = result;
-    });
-
-    this.lookupService.retrieveReportingPeriod()
-    .subscribe(result => {
-      const annual = result.find(period => period.code === 'A');
-      this.reportingPeriodValues.push(annual);
-      this.reportingPeriodForm.get('reportingPeriodTypeCode').patchValue(annual);
-    });
-
-    this.lookupService.retrieveUom()
-    .subscribe(result => {
-      this.uomValues = result;
-      this.denominatorUomValues = this.uomValues.filter(val => val.efDenominator);
-    });
-
-    this.lookupService.retrieveFuelUseUom()
-    .subscribe(result => {
-      this.fuelUseUomValues = result;
-      this.heatContentUomValues = this.fuelUseUomValues.filter(val => val.heatContentUom);
-    });
-
-    if (this.sccCode) {
-      this.getPointSourceScc(this.sccCode);
+    constructor(
+        private lookupService: LookupService,
+        public formUtils: FormUtilsService,
+        private sharedService: SharedService,
+        private fb: FormBuilder) {
     }
 
-    this.sharedService.processSccChangeEmitted$.subscribe(scc => {
-      if (scc) {
-        this.getPointSourceScc(scc);
-      }
-    });
+    ngOnInit() {
 
-  }
+        this.lookupService.retrieveCalcMaterial()
+            .subscribe(result => {
+                this.materialValues = result;
+            });
 
-  ngOnChanges() {
+        this.lookupService.retrieveFuelUseMaterial()
+            .subscribe(result => {
+                this.fuelUseMaterialValues = result;
+            });
 
-    this.reportingPeriodForm.reset(this.reportingPeriod);
-  }
+        this.lookupService.retrieveCalcParam()
+            .subscribe(result => {
+                this.parameterTypeValues = result;
+            });
 
-  onSubmit() {
+        this.lookupService.retrieveEmissionsOperatingType()
+            .subscribe(result => {
+                this.operatingStatusValues = result;
+            });
 
-    // let period = new ReportingPeriod();
-    // Object.assign(period, this.reportingPeriodForm.value);
-    // console.log(period);
-  }
+        this.lookupService.retrieveReportingPeriod()
+            .subscribe(result => {
+                const annual = result.find(period => period.code === 'A');
+                this.reportingPeriodValues.push(annual);
+                this.reportingPeriodForm.get('reportingPeriodTypeCode').patchValue(annual);
+            });
 
-  getPointSourceScc(processScc: string) {
-    this.lookupService.retrievePointSourceSccCode(processScc)
-      .subscribe(result => {
-        if (result && result.fuelUseRequired) {
-          this.isFuelUseScc = true;
-          this.reportingPeriodForm.get('fuelUseValue').updateValueAndValidity();
-        } else {
-          this.isFuelUseScc = false;
+        this.lookupService.retrieveUom()
+            .subscribe(result => {
+                this.uomValues = result;
+                this.denominatorUomValues = this.uomValues.filter(val => val.efDenominator);
+            });
+
+        this.lookupService.retrieveFuelUseUom()
+            .subscribe(result => {
+                this.fuelUseUomValues = result;
+                this.heatContentUomValues = this.fuelUseUomValues.filter(val => val.heatContentUom);
+            });
+
+        if (this.sccCode) {
+            this.getPointSourceScc(this.sccCode);
         }
 
-      });
-  }
+        this.sharedService.processSccChangeEmitted$.subscribe(scc => {
+            if (scc) {
+                this.getPointSourceScc(scc);
+            }
+        });
 
-  checkHeatCapacityUom(): ValidatorFn {
-    return (control: FormGroup): ValidationErrors | null => {
-      const heatContentValue = control.get('heatContentValue').value;
-      const heatContentUom = control.get('heatContentUom').value;
+    }
 
-      if (this.isFuelUseScc && (!heatContentValue || !heatContentUom)) {
-        return {sccHeatContentfields: true};
-      }
-      if (!this.isFuelUseScc && ((heatContentValue && !heatContentUom) || (!heatContentValue && heatContentUom))) {
-        return {heatContentInvalid: true};
-      }
-      return null;
-    };
-  }
+    ngOnChanges() {
 
-  checkFuelUseFields(): ValidatorFn {
-    return (control: FormGroup): ValidationErrors | null => {
-      const fuelValue = control.get('fuelUseValue').value;
-      const fuelMaterial = control.get('fuelUseMaterialCode').value;
-      const fuelUom = control.get('fuelUseUom').value;
+        this.reportingPeriodForm.reset(this.reportingPeriod);
+    }
 
-      if (this.isFuelUseScc) {
-        this.sccFuelUsefieldsWarning = 'Fuel data for this Process SCC must be reported. If this is a duplicate process for an Alternative Throughput, only report Fuel data for one of these Processes.';
-      }
+    onSubmit() {
 
-      if (!this.isFuelUseScc && (fuelValue || fuelMaterial || fuelUom)
-        && (fuelValue === null || fuelValue === '' || !fuelMaterial || !fuelUom)) {
-        return {fuelUsefields: true};
-      }
-      return null;
-    };
-  }
+        // let period = new ReportingPeriod();
+        // Object.assign(period, this.reportingPeriodForm.value);
+        // console.log(period);
+    }
 
-  copyFuelDataToThroughput() {
-      const fuelMaterial = this.reportingPeriodForm.get('fuelUseMaterialCode').value;
-      const fuelValue = this.reportingPeriodForm.get('fuelUseValue').value;
-      const fuelUom = this.reportingPeriodForm.get('fuelUseUom').value
+    getPointSourceScc(processScc: string) {
+        this.lookupService.retrievePointSourceSccCode(processScc)
+            .subscribe(result => {
+                if (result && result.fuelUseRequired) {
+                    this.isFuelUseScc = true;
+                    this.reportingPeriodForm.get('fuelUseValue').updateValueAndValidity();
+                } else {
+                    this.isFuelUseScc = false;
+                }
 
-      this.reportingPeriodForm.get('calculationMaterialCode').patchValue(fuelMaterial);
-      this.reportingPeriodForm.get('calculationParameterValue').patchValue(fuelValue);
-      this.reportingPeriodForm.get('calculationParameterUom').patchValue(fuelUom);
-      this.showFuelDataCopyMessage = true;
+            });
+    }
 
-  }
+    checkHeatCapacityUom(): ValidatorFn {
+        return (control: FormGroup): ValidationErrors | null => {
+            const heatContentValue = control.get('heatContentValue').value;
+            const heatContentUom = control.get('heatContentUom').value;
 
-  isFuelUseDirty = ():boolean => {
-      const fuelMaterial = this.reportingPeriodForm.get('fuelUseMaterialCode');
-      const fuelValue = this.reportingPeriodForm.get('fuelUseValue');
-      const fuelUom = this.reportingPeriodForm.get('fuelUseUom');
+            if (this.isFuelUseScc && (!heatContentValue || !heatContentUom)) {
+                return {sccHeatContentfields: true};
+            }
+            if (!this.isFuelUseScc && ((heatContentValue && !heatContentUom) || (!heatContentValue && heatContentUom))) {
+                return {heatContentInvalid: true};
+            }
+            return null;
+        };
+    }
 
-      return fuelMaterial.dirty && fuelValue.dirty && fuelUom.dirty;
+    checkFuelUseFields(): ValidatorFn {
+        return (control: FormGroup): ValidationErrors | null => {
+            const fuelValue = control.get('fuelUseValue').value;
+            const fuelMaterial = control.get('fuelUseMaterialCode').value;
+            const fuelUom = control.get('fuelUseUom').value;
 
-  }
+            if (this.isFuelUseScc) {
+                this.sccFuelUsefieldsWarning = 'Fuel data for this Process SCC must be reported. If this is a duplicate process for an Alternative Throughput, only report Fuel data for one of these Processes.';
+            }
+
+            if (!this.isFuelUseScc && (fuelValue || fuelMaterial || fuelUom)
+                && (fuelValue === null || fuelValue === '' || !fuelMaterial || !fuelUom)) {
+                return {fuelUsefields: true};
+            }
+            return null;
+        };
+    }
+
+    copyFuelDataToThroughput() {
+        const fuelMaterial = this.reportingPeriodForm.get('fuelUseMaterialCode').value;
+        const fuelValue = this.reportingPeriodForm.get('fuelUseValue').value;
+        const fuelUom = this.reportingPeriodForm.get('fuelUseUom').value
+
+        this.reportingPeriodForm.get('calculationMaterialCode').patchValue(fuelMaterial);
+        this.reportingPeriodForm.get('calculationParameterValue').patchValue(fuelValue);
+        this.reportingPeriodForm.get('calculationParameterUom').patchValue(fuelUom);
+        this.showFuelDataCopyMessage = true;
+        setTimeout(() => {
+            this.showFuelDataCopyMessage = false;
+        }, 6000);
+
+    }
+
+    isFuelUseDirty = (): boolean => {
+        const fuelMaterial = this.reportingPeriodForm.get('fuelUseMaterialCode');
+        const fuelValue = this.reportingPeriodForm.get('fuelUseValue');
+        const fuelUom = this.reportingPeriodForm.get('fuelUseUom');
+
+        return fuelMaterial.dirty && fuelValue.dirty && fuelUom.dirty;
+
+    }
 
 }
