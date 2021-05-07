5307678ec7199b8c16b0b555ed3f4d7045281202
Thomas Fesperman <thomas.fesperman@cgifederal.com>
2021-05-07 14:42:33 -0400

    [CEF-1327] Adding clean handling of IOExceptions when uploading attachments

--- cef-web/src/main/java/gov/epa/cef/web/api/rest/ReportAttachmentApi.java
+++ cef-web/src/main/java/gov/epa/cef/web/api/rest/ReportAttachmentApi.java
@@ -145,7 +145,13 @@ public class ReportAttachmentApi {
 
         } catch (IOException e) {
 
-            throw new IllegalStateException(e);
+            //throw new IllegalStateException(e);
+            String msg = String.format("There was an issue during file upload. Please try again. If you continue to experience issues, "
+                    + "please ensure that your file is not infected with a virus and reach out to the helpdesk.",
+                file.getOriginalFilename(), e.getMessage());
+            
+            throw new ReportAttachmentValidationException(
+                    Collections.singletonList(WorksheetError.createSystemError(msg)));
         }
     	
     	return new ResponseEntity<>(result, status);
