a30ddb9075380fcf39727125e91899a72940fddc
Lee <anne.lee1@cgifederal.com>
2021-10-05 09:32:25 -0400

    CEF-1293 update to remove redundant statements

--- cef-web/src/main/resources/db/migrations/V3.0.2__AddSLTBannerProperty.sql
+++ cef-web/src/main/resources/db/migrations/V3.0.2__AddSLTBannerProperty.sql
@@ -1,6 +1,5 @@
 
 ALTER TABLE slt_properties ADD COLUMN required boolean NOT NULL DEFAULT true;
-ALTER TABLE slt_properties ALTER COLUMN required SET DEFAULT true;
 
 INSERT INTO slt_properties (name, label, description, datatype, required) values (
 'slt-feature.announcement.enabled','SLT Announcement Banner','Enables the SLT announcement banner visible users associated with SLT in the CAER application.','boolean', true);
@@ -9,7 +8,6 @@ INSERT INTO slt_properties (name, label, description, datatype, required) values
 'slt-feature.announcement.text','SLT Announcement Banner Message','Text shown in the SLT announcement banner.','textarea', false);
 
 ALTER TABLE admin_properties ADD COLUMN required boolean NOT NULL DEFAULT true;
-ALTER TABLE admin_properties ALTER COLUMN required SET DEFAULT true;
 
 UPDATE admin_properties SET required = false, datatype = 'textarea' WHERE name = 'feature.announcement.text';
 UPDATE admin_properties SET datatype = 'textarea' WHERE name = 'email.admin';
